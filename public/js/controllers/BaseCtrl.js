23f4dac228a7b5af23572a9bf6158f22a8442656
chr22 <chrmaui@gmail.com>
2014-01-04 00:06:04 +0100

    added back function

--- public/js/controllers/BaseCtrl.js
+++ public/js/controllers/BaseCtrl.js
@@ -1,5 +1,8 @@
 'use strict';
 
-myApp.controller('BaseCtrl', ["$scope", "$http", function BaseCtrl($scope, $http) {
-
+myApp.controller('BaseCtrl', ["$scope", "$window", function BaseCtrl($scope, $window) {
+	$scope.back = function() {
+		console.log("back");
+		$window.history.back();
+	};
 }]);
