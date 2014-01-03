d857ea45ad32ab1820f923db6ea49df4ab8834a6
chr22 <chrmaui@gmail.com>
2014-01-03 23:48:04 +0100

    bug fixes

--- public/js/controllers/posts/ReadPostCtrl.js
+++ public/js/controllers/posts/ReadPostCtrl.js
@@ -1,10 +1,18 @@
 'use strict';
 
-myApp.controller('ReadPostCtrl', ["$scope", "$routeParams", "PostService", function ReadPostCtrl($scope, $routeParams, PostService) {
+myApp.controller('ReadPostCtrl', ["$scope", "$routeParams", "PostService", "$location", function ReadPostCtrl($scope, $routeParams, PostService, $location) {
 	$scope.post = {};
 
 	$scope.ReadPost = function() {
 		$scope.post = PostService.GetPost($routeParams.id);
 	}();
+	
+	$scope.EditPost = function(id) {
+		$location.path("/editPost/" + id);
+	};
+	
+	$scope.DeletePost = function(id) {
+		$location.path("/deletePost/" + id);
+	};
 
 }]);
