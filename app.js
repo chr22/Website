3fa5c1c345db723c82c14972973fb89766cb76eb
chr22 <chrmaui@gmail.com>
2014-02-17 17:53:44 +0100

    test

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ module.exports = function (db) {
 	// export NODE_ENV=production
 	if (app.get('env') === 'production') {
 		// Add www to url
+		console.log("production");
 		app.get('*', function(req, res, next) {
 			if (req.headers.host.slice(0, 3) != 'www') {				
 				res.redirect('http://www.' + req.headers.host + req.url, 301);	
