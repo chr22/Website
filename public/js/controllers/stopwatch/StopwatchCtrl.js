5f13a46c82ede9d0ec08e0b8aae751cc743d1302
chr22 <chrmaui@gmail.com>
2014-02-17 18:28:58 +0100

    added files to stopwatch

---
+++ public/js/controllers/stopwatch/StopwatchCtrl.js
@@ -0,0 +1,13 @@
+(function () {
+	'use strict';
+
+	myApp.controller('StopWatchCtrl', ["$scope", "$location", "$routeParams", function StopWatchCtrl($scope, $location, $routeParams) {
+		
+		$scope.time = 0;
+		
+		$scope.Start = function() {
+				
+		};
+		
+	}]);
+}());
