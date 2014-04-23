ba8b3f2274c8e342300ebafb8cd1dcc416b8ed5e
chr22 <chrmaui@gmail.com>
2014-04-24 00:01:21 +0200

    hundred beer backend

---
+++ routes/hundredBeers.js
@@ -0,0 +1,62 @@
+(function() {
+	'use strict';
+	
+	var db = require('../db/db.js'),
+		Beer = require('../db/schemas/hundredBeers');
+	
+	exports.status = function (req, res) {		
+		Beer.find(function (err, result) {
+			if (err) throw err;
+			res.json({
+				status: result
+			});
+		});
+	};
+	
+	// POST
+	exports.addPerson = function (req, res) {		
+		var person = new Beer({
+			Name: req.body.Name,
+			Beers: req.body.Beers
+		});
+
+		person.save(function(err, result) {
+			if(err) throw err;
+			res.json(result);
+		});
+	};
+	
+	exports.addBeer = function(req, res) {
+		var id = req.body._id;
+		delete req.body._id;
+		req.body.Beers++;
+		
+		Beer.findOneAndUpdate({_id: id}, req.body, function(err, result) {
+			if (err) throw err;
+			res.json({
+				person: result
+			});
+		});
+	};
+	
+	exports.subtractBeer = function(req, res) {
+		var id = req.body._id;
+		delete req.body._id;
+		req.body.Beers--;
+		
+		Beer.findOneAndUpdate({_id: id}, req.body, function(err, result) {
+			if (err) throw err;
+			res.json({
+				person: result
+			});
+		});
+	};
+	
+	exports.deletePerson = function(req, res) {
+		Beer.remove({_id: req.params.id}, function(err) {
+			if (err) throw err;
+			res.json(true);
+		});
+	};
+	
+}());
